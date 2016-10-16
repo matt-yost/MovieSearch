0ae24306f8ea986426eda477faf542f8876c603b
Matt <matt.yost2@gmail.com>
2016-10-16 17:45:52 -0500

    Comments
    Comments

--- js/app.js
+++ js/app.js
@@ -24,12 +24,16 @@
     $.get(url, options, displaySearch);
   };
 
+  // Function that takes the results from OMDb and dynamically displays them
   var displaySearch = function(results){
     var displayMoviesHTML = "";
+
+    // If there is a response loop through the results and display
     if(results.Response === "True"){
       $.each(results.Search, function(i, movie){
-        displayMoviesHTML += "<li><div class='poster-wrap'>"
+        displayMoviesHTML += "<li><div class='poster-wrap'>";
 
+        // If the movie poster is not available, display the default placeholder. Otherwise, display poster
         if (movie.Poster === 'N/A') {
     			displayMoviesHTML += "<i class='material-icons poster-placeholder'>crop_original</i>";
     		} else {
@@ -37,16 +41,17 @@
     		}
 
         displayMoviesHTML += "</div>";
+        // Display title and year of release
         displayMoviesHTML += "<span class='movie-title'>" + movie.Title + "</span>";
         displayMoviesHTML += "<span class='movie-year'>" + movie.Year + "</span></li>";
       });
+      // If there is no results, let the user know
     } else {
       displayMoviesHTML += "<li class='no-movies'>";
       displayMoviesHTML += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + searchTitle + "</li>";
     }
     $('#movies').html(displayMoviesHTML);
   };
-
 
 // Don't even try it
 }());
