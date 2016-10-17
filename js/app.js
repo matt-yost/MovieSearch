27dfaab39af446156c9ac3e19299abe973e31cc8
Matt <matt.yost2@gmail.com>
2016-10-16 21:50:43 -0500

    Functional Description
    Display function and get request are functional

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
 (function() {
   'use strict';
 
+  $(".description").hide();
+
   // Define url for GET Request
   var url = "http://www.omdbapi.com/?";
-  var searchTitle, searchYear;
+  var searchTitle, searchYear,userSelectedTitle, userSelectedYear;
 
   // On form submit, capture user input and initiate GET request
   $("form").submit(function(e){
     e.preventDefault();
     searchTitle = $("#search").val();
     searchYear = $("#year").val();
-    getOMDb();
+    getMovies();
   });
 
-  // Send request to OMDb
-  var getOMDb = function(){
+  // Send request to OMDb for movies to display
+  var getMovies = function(){
     var options = {
       s: searchTitle,
+      y: searchYear,
       type: "movie",
       r: 'json'
     };
@@ -26,32 +29,78 @@
 
   // Function that takes the results from OMDb and dynamically displays them
   var displaySearch = function(results){
-    var displayMoviesHTML = "";
+    var html = "";
 
     // If there is a response loop through the results and display
     if(results.Response === "True"){
       $.each(results.Search, function(i, movie){
-        displayMoviesHTML += "<li><div class='poster-wrap'>";
+        html += "<li><div class='poster-wrap'>";
 
-        // If the movie poster is not available, display the default placeholder. Otherwise, display poster
+        // If the movie poster is not available, display the default placeholder. Otherwise, display poster and link to IMDB
         if (movie.Poster === 'N/A') {
-    			displayMoviesHTML += "<i class='material-icons poster-placeholder'>crop_original</i>";
+    			html += "<i class='material-icons poster-placeholder'>crop_original</i>";
     		} else {
-    			displayMoviesHTML += "<img class='movie-poster' src='" + movie.Poster + "'>";
+    			html += "<a href='http://www.imdb.com/title/" + movie.imdbID + "'>" + "<img class='movie-poster' src='" + movie.Poster + "'>";
     		}
 
-        displayMoviesHTML += "</div>";
+        html += "</a></div>";
         // Display title and year of release
-        displayMoviesHTML += "<span class='movie-title'>" + movie.Title + "</span>";
-        displayMoviesHTML += "<span class='movie-year'>" + movie.Year + "</span></li>";
+        html += "<span class='movie-title'>" + movie.Title + "</span>";
+        html += "<span class='movie-year'>" + movie.Year + "</span>";
+        html += "<button type='button' class='description-button'>Description</button></li>";
       });
       // If there is no results, let the user know
     } else {
-      displayMoviesHTML += "<li class='no-movies'>";
-      displayMoviesHTML += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + searchTitle + "</li>";
+      html += "<li class='no-movies'>";
+      html += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + searchTitle + "</li>";
     }
-    $('#movies').html(displayMoviesHTML);
+    $("#movies").html(html);
   };
 
-// Don't even try it
+  // When a description button is clicked, gather information to send request
+  $("body").on("click", ".description-button", function(){
+    userSelectedTitle = $(this).prev().prev().text();
+    userSelectedYear = $(this).prev().text();
+
+    getDescription();
+  });
+
+  // Send request to OMDb to show descripton of movie selected
+  var getDescription = function(){
+    var options = {
+      t: userSelectedTitle,
+      y: userSelectedYear,
+      plot: "full",
+      type: "movie",
+      r: "json"
+    }
+
+    $.get(url, options, displayDescription);
+  };
+
+  // Dynamically display description results
+  var displayDescription = function(results){
+    $(".main-content").hide();
+    $(".description").show();
+    var html = "";
+    html += "<div class='description'><div class='title-bar'><a href='index.html'><p id='search-link'>Search results</p></a>";
+    html += "<h1 id='description-movie-title'>" + results.Title + "(" + results.Year + ")" + "</h1>";
+    html += "<p id='imdb-rating'>IMDB Rating: " + results.imdbRating + "</p></div>";
+    html += "<div class='content-bar'>";
+
+    if(results.Poster === "N/A"){
+      html += "<p id='movie-poster'>Poster not available</p>";
+    } else {
+      html += "<img id='movie-poster' src='" + results.Poster + "'/>";
+    }
+
+    html += "<h3 id='plot-synopsis'>Plot Synopsis:</h3>";
+    html += "<p id='plot-syn-text'>" + results.Plot + "</p>";
+    html += "<a id='view-on-imdb' href='http://www.imdb.com/title/" + results.imdbID + "'>View on IMDB</a></div></div>";
+
+    $(".description").html(html);
+  }
+
+
+// Do not type below here
 }());
