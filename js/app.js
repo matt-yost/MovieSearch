8385985787711898b68b5750e3663323ebefe6c2
Matt <matt.yost2@gmail.com>
2016-10-16 23:03:55 -0500

    Cleanup
    Cleanup

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@
 
   $(".description").hide();
 
-  // Define url for GET Request
+  // Define url for GET Request and global variables
   var url = "http://www.omdbapi.com/?";
   var searchTitle, searchYear,userSelectedTitle, userSelectedYear;
 
   // On form submit, capture user input and initiate GET request
-  $("form").submit(function(e){
+  $("body").on("submit", "form", function(e){
+    $(".description").hide();
+    $(".main-content").fadeIn();
     e.preventDefault();
     searchTitle = $("#search").val();
     searchYear = $("#year").val();
@@ -28,12 +30,12 @@
   };
 
   // Function that takes the results from OMDb and dynamically displays them
-  var displaySearch = function(results){
+  var displaySearch = function(res){
     var html = "";
 
     // If there is a response loop through the results and display
-    if(results.Response === "True"){
-      $.each(results.Search, function(i, movie){
+    if(res.Response === "True"){
+      $.each(res.Search, function(i, movie){
         html += "<li><div class='poster-wrap'>";
 
         // If the movie poster is not available, display the default placeholder. Otherwise, display poster and link to IMDB
@@ -49,15 +51,18 @@
         html += "<span class='movie-year'>" + movie.Year + "</span>";
         html += "<button type='button' class='description-button'>Description</button></li>";
       });
-      // If there is no results, let the user know
+
+    // If there is no results, let the user know
     } else {
       html += "<li class='no-movies'>";
       html += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + searchTitle + "</li>";
     }
+
+    // update HTML with response
     $("#movies").html(html);
   };
 
-  // When a description button is clicked, gather information to send request
+  // When a description button is clicked, send request with for the title that the user clicked
   $("body").on("click", ".description-button", function(){
     userSelectedTitle = $(this).prev().prev().text();
     userSelectedYear = $(this).prev().text();
@@ -73,34 +78,37 @@
       plot: "full",
       type: "movie",
       r: "json"
-    }
+    };
 
     $.get(url, options, displayDescription);
   };
 
-  // Dynamically display description results
-  var displayDescription = function(results){
+  // Dynamically display description response
+  var displayDescription = function(res){
     $(".main-content").hide();
-    $(".description").show();
+    $(".description").fadeIn();
     var html = "";
-    html += "<div class='description'><div class='title-bar'><a href='index.html'><p id='search-link'>Search results</p></a>";
-    html += "<h1 id='description-movie-title'>" + results.Title + "(" + results.Year + ")" + "</h1>";
-    html += "<p id='imdb-rating'>IMDB Rating: " + results.imdbRating + "</p></div>";
+    html += "<div class='description'><div class='title-bar'><a href='index.html'><p id='search-link'>New Search</p></a>";
+    html += "<h1 id='description-movie-title'>" + res.Title + "(" + res.Year + ")" + "</h1>";
+    html += "<p id='imdb-rating'>IMDB Rating: " + res.imdbRating + "</p></div>";
     html += "<div class='content-bar'>";
 
-    if(results.Poster === "N/A"){
+    // If there is no poster then tell the user.
+    if(res.Poster === "N/A"){
       html += "<p id='movie-poster'>Poster not available</p>";
     } else {
-      html += "<img id='movie-poster' src='" + results.Poster + "'/>";
+      html += "<img id='movie-poster' src='" + res.Poster + "'/>";
     }
 
+    // Display plot and link to IMDb page
     html += "<h3 id='plot-synopsis'>Plot Synopsis:</h3>";
-    html += "<p id='plot-syn-text'>" + results.Plot + "</p>";
-    html += "<a id='view-on-imdb' href='http://www.imdb.com/title/" + results.imdbID + "'>View on IMDB</a></div></div>";
+    html += "<p id='plot-syn-text'>" + res.Plot + "</p>";
+    html += "<a id='view-on-imdb' href='http://www.imdb.com/title/" + res.imdbID + "'>View on IMDB</a></div></div>";
 
+    // Update HTML with response
     $(".description").html(html);
-  }
+  };
 
 
-// Do not type below here
+// Don't
 }());
